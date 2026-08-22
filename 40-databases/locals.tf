e7da718647739bb5c472595ab8e51345d57475c4
Gopal <gopalakrishna.gunji@gmail.com>
2026-08-22 03:10:17 -0400

    infra

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -7,5 +7,6 @@ locals {
     }
     # public subnet in 1a AZ
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id
+    #mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
