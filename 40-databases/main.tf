925b088d499a642d0d584ec98c83474aacf349e0
Gopal <gopalakrishna.gunji@gmail.com>
2026-08-22 03:00:59 -0400

    infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -26,15 +26,16 @@ resource "terraform_data" "bootstrap" {
     host        =aws_instance.mongodb.private_ip
   }
 
-
   provisioner "file" {
-  source      = "bootstrap.sh"
-  destination = "/tmp/bootstrap.sh"
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
 
   provisioner "remote-exec" {
     inline = [
-        "chmod +x /tmp/bootstrap.sh"
-        "sudo sh /tmp/bootstrap.sh"
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"
     ]
   }
 }
+
