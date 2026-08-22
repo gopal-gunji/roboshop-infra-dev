798643878c4365929096aef266b8ee3d4aba6eaf
Gopal <gopalakrishna.gunji@gmail.com>
2026-08-22 04:47:26 -0400

    infra

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -7,3 +7,11 @@ resource "aws_route53_record" "mongodb" {
   allow_overwrite = true
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
