d69a46247b41b5fb38f4a1f28f5eb5a7019c356f
Gopal <gopalakrishna.gunji@gmail.com>
2026-08-23 05:09:11 -0400

    infra

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -26,8 +26,8 @@ resource "aws_iam_role" "mysql" {
 resource "aws_iam_policy" "mysql" {
   name        = local.mysql_policy_name
   description = "A policy for mysql EC2 instance"
-  policy      = templatefile("mysql-iam-policy.json", {
-            environment = var.environment
+  policy = templatefile("mysql-iam-policy.json", {
+    environment = var.environment
   })
 }
 
