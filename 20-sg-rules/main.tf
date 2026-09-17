fff73ef837824969b6407f3c1713807539e70e39
Gopal <gopalakrishna.gunji@gmail.com>
2026-09-17 19:44:03 -0400

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -350,6 +350,6 @@ resource "aws_security_group_rule" "backend_alb_openvpn" {
   to_port   = 80
   protocol  = "tcp"
   # where traffic is comming from
-  source_security_group_id = local.backend_alb_sg_id
-  security_group_id        = local.openvpn_sg_id
+  source_security_group_id = local.openvpn_sg_id
+  security_group_id        = local.backend_alb_sg_id
 }
